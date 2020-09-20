d2a61be00ec707cbf52e2629e2d7b27fc39ce152
Nishant Sharma <4unishant@gmail.com>
2020-09-20 14:52:59 +0530

    code updated

--- routes/users.js
+++ routes/users.js
@@ -122,4 +122,97 @@ router.post("/login", async function (req, res) {
   }
 });
 
+router.post("/validate", async function (req, res) {
+  try {
+    var client = await mongoClient.connect(url);
+    var db = client.db("user-login");
+    var user = await db.collection("user").findOne({ email: req.body.email });
+    if (!user) 
+    {
+      const data = `https://bit.ly/2FNnNC2/reset.html`;
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: "fullstack.webtesting@gmail.com", // generated ethereal user
+          pass: pwd, // generated ethereal password
+        },
+        tls: {
+          // do not fail on invalid certs
+          rejectUnauthorized: false
+        }
+      });
+
+      let mailOptions={
+        from: '"Nishant" <fullstack.webtesting@gmail.com>', // sender address
+        to: "fullstack.webtesting@gmail.com", // list of receivers
+        subject: "testing...", // Subject line
+        text: "Hello world?", // plain text body
+        html: data // html body
+      };
+    
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions,(error,info)=>{
+        if(error)
+        {
+          console.log("error: "+error);
+        }
+        else
+        {
+          console.log("Message sent: %s", info.messageId);
+          console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        }
+      });
+    }
+    else
+    {
+      alert("Email aleady registrered!");
+    }
+
+  }
+  catch (error) {
+    console.log("ERROR: " + error);
+    res.json({
+      message: "Something went wrong: " + error
+    })
+  }
+});
+
+/*router.post("/reset", async function (req, res) {
+  try {
+    var client = await mongoClient.connect(url);
+    var db = client.db("user-login");
+    //find the user with email
+    var user = await db.collection("user").findOne({ email: req.body.email });
+    if (user) {
+      //comapare the password
+      var result = await bcryptjs.compare(req.body.password, user.password);
+      if (result) {
+        //alert("ACCESS GRANTED :)");
+        res.json({
+          message: "ACCESS GRANTED :)"
+        });
+      }
+      else {
+        //alert("ACCESS DENIED :( (incorrect username/password");
+        res.json({
+          message: "ACCESS DENIED :( (incorrect username/password)"
+        });
+      }
+    }
+    else {
+      //alert("No such user exists, kindly register yourself!!!!");
+      res.json({
+        message: "No such user exists, kindly register yourself!!!!"
+      });
+    }
+  }
+  catch (error) {
+    res.json({
+      message: "Something went wrong: " + error
+    })
+  }
+});*/
+
 module.exports = router;
