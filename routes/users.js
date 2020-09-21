37f2315426a71689952928b3b56adbe83596c282
Nishant Sharma <4unishant@gmail.com>
2020-09-21 11:19:00 +0530

    updated code

--- routes/users.js
+++ routes/users.js
@@ -39,8 +39,8 @@ router.post("/register", async function (req, res) {
       `;
       let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
           user: "fullstack.webtesting@gmail.com", // generated ethereal user
           pass: pwd, // generated ethereal password
