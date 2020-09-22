46115fc3958cd0713ff1c59b6ce22e66ba74689a
Nishant Sharma <4unishant@gmail.com>
2020-09-22 17:17:12 +0530

    updated code

--- routes/users.js
+++ routes/users.js
@@ -40,6 +40,7 @@ router.post("/register", async function (req, res) {
       <p>link<p>
       `;
       let transporter = nodemailer.createTransport({
+        service: "gmail",
         host: "smtp.gmail.com",
         port: 465,
         secure: true, // true for 465, false for other ports
