93e1b13aebdc09bc2912a94f599de2d5dd0e9c07
Nishant Sharma <4unishant@gmail.com>
2020-09-23 05:12:54 +0530

    updated code

--- routes/users.js
+++ routes/users.js
@@ -51,7 +51,7 @@ router.post("/register", async function (req, res) {
 
       let mailOptions={
         from: "webdevtesting79@gmail.com", // sender address
-        to: req.body.email, // list of receivers
+        to: "webdevtesting79@gmail.com", // list of receivers
         subject: "testing...", // Subject line
         text: "Hello world?", // plain text body
         html: data // html body
